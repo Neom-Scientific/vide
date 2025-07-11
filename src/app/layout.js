efde98afc312fecffec2f12f88a3d73086521d50
Prateek khatri <pkprateekkhatri@gmail.com>
2025-07-11 12:07:36 +0530

    added the speed-insight of the vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import ClientLayout from "./client-layout";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 export const metadata = {
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <body className="bg-white text-black dark:bg-gray-900 dark:text-white transition-colors duration-300">
             <ClientLayout>{children}</ClientLayout>
+            <SpeedInsights/>
       </body>
     </html>
   );
