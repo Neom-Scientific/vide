5208ba23cadb928849dda22defce1eb9687915df
Prateek khatri <pkprateekkhatri@gmail.com>
2025-08-11 16:27:49 +0530

    fix the cors issue

--- src/app/api/request-insert/route.js
+++ src/app/api/request-insert/route.js
@@ -126,7 +126,7 @@ export async function POST(request) {
     }
     catch (error) {
         console.error('Error executing query', error);
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500, headers: corsHeaders });
     }
 }
 
